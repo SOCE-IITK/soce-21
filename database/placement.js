aa12c98d9d52f0261f648c5d941934554c60fdac
Aman <amanks20@iitk.ac.in>
2021-12-06 11:24:59 +0530

    add thumbnail link to placement.js database

--- database/placement.js
+++ database/placement.js
@@ -4,63 +4,72 @@ var chats = [
         work: "IOCL",
         text: "Let me first introduce myself. I am Jeetendra Kumar Patel (Y16), B.Tech in CE, IITK. Currently, I am working in IOCL, I got placed in Phase 2 last year. ",
         dp: "/img/Student-section/placement/jeetendra_y16.jpg",
-        link:"/Jeetendra_Kumar_Patel"
+        link:"/Jeetendra_Kumar_Patel",
+        thumbnail:"/img/Student-section/placement/thumbnail/1.jpg",
     },
     {
         name: "Shuaib Alam (Y18)",
         work: "Hero Future Energies (New Delhi)",
         text: "Hello Everyone! Myself SHUAIB ALAM. I completed my Graduation from JAMIA MILLIA ISLAMIA, NEW DELHI in CIVIL ENGINEERING.",
         dp: "/img/Student-section/placement/Shuaib-Alam.jpeg",
-        link:"/Shuaib_Alam"
+        link:"/Shuaib_Alam",
+        thumbnail:"/img/Student-section/placement/thumbnail/2.jpg",
     },
     {
         name: "Chitwan Garg (Y16)",
         work: "HEC ",
         text: "Hello Everyone! Myself Chitwan Garg Y16, B-Tech graduate in Civil Engineering and a Double Major in Economics. I am a guy who did not have any significant extracurricular involvements during my first year except for those that were made compulsory to remain patriotic to our mother hall with hearts full of hall feeling :p (I enjoyed the escapes from HEC that I made with some of my wingies during takneek).",
         dp: "/img/Student-section/placement/chitwan-garg-y17.jpg",
-        link:"/Chitwan_Garg"
+        link:"/Chitwan_Garg",
+        thumbnail:"/img/Student-section/placement/thumbnail/3.jpg",
     },
     {
         name: "Ashutosh Ranjan (Y17)",
         work: "TATA Projects Limited",
         text: "Hello Everyone! My name is Ashutosh Ranjan, Y17, BTech in Civil Engineering, IITK. First, I will give a brief introduction about my curriculum. I am a fourth-year student and got placed in TATA Projects Limited in placement season 2020-21.",
         dp: "/img/Student-section/placement/Ashutosh-Ranjan.jpeg",
-        link:"/Ashutosh_Ranjan"
+        link:"/Ashutosh_Ranjan",
+        thumbnail:"/img/Student-section/placement/thumbnail/4.jpg",
     },
     {
         name: "Dishaa Rajora ",
         work: "PWC",
         text: "Before I begin with the story of my placement journey, let us have introductions first. Hi. I am Dishaa Rajora, final year undergraduate majoring in Civil Engineering (wow, this introduction gave me formal-email-to-professor-don’t-know-how-to-begin-the-email feel).",
         dp: "/img/Student-section/placement/dishaa-rajora.jpg",
-        link:"/Dishaa_Rajora"
+        link:"/Dishaa_Rajora",
+        thumbnail:"/img/Student-section/placement/thumbnail/5.jpg",
     },
     {
         name: "Krishan Kumar (Y17)",
         work: "Sterlite Technology",
         text: "Starting with a brief intro of mine, I am Krishan Kumar, Y17 BT Civil Engineering. I got Placed on Day 2 in a Company named Sterlite Technology in my very first interview.",
         dp: "/img/Student-section/placement/Krishan-Kumar.jpg",
-        link:"/Krishan_Kumar"
+        link:"/Krishan_Kumar",
+        thumbnail:"/img/Student-section/placement/thumbnail/6.jpg",
     },
     {
         name: "Vishal Agarwal (Y17)",
         work: "MTX",
         text: "Hello Everyone! My name is Vishal Agarwal (Y17, CE), currently placed at MTX Group and today I am going to give you a student’s perspective, something really honest and straightforward because that’s an element often missing in the talks that are given by the companies.",
         dp: "/img/Student-section/placement/Vishal-Agarwal.jpg",
-        link:"/Vishal_Agarwal"
+        link:"/Vishal_Agarwal",
+        thumbnail:"/img/Student-section/placement/thumbnail/7.jpg",
     },
     {
         name: "Shashi Shekhar (Y17)",
         work: "Blackrock",
         text: "Hi people! Let me share my story. So, I am Shashi Shekhar. I got placed in Blackrock on Day 1 during the placement season 20-21. My extra-curricular involvements changed with every year, but the major ones have been Manager- Research, Academics and Career Council and Senator, Y17.",
         dp: "/img/Student-section/placement/Shashi-Shekhar.jpg",
-        link:"/Shashi_Shekhar"
+        link:"/Shashi_Shekhar",
+        thumbnail:"/img/Student-section/placement/thumbnail/8.jpg",
     },
     {
         name: "Samyak Jain (Y17)",
         work: "BFSI",
         text: "Hello Everyone! I am Samyak Jain, a final year undergraduate in Civil Engineering. I am currently placed at Navi technologies, a startup in the BFSI industry, as a Software developer.",
         dp: "/img/Student-section/placement/Samyak-Jain.jpg",
-        link:"/Samyak_Jain"
+        link:"/Samyak_Jain",
+        thumbnail:"/img/Student-section/placement/thumbnail/9.jpg",
     },
     {
         name: "Rajat Ghoshal (Y17)",
@@ -68,48 +77,55 @@ var chats = [
         text: "Hello Everyone! I am Rajat Ghoshal, a Final Year Undergraduate (Y17) in Civil Engineering, hoping for minors in IME and ML (a retrospective one :p). About academics - with a CPI just enough for distinction",
         dp: "/img/Student-section/placement/Rajat-Ghoshal.jpeg",
         link:"/Rajat_Ghoshal",
+        thumbnail:"/img/Student-section/placement/thumbnail/10.jpg",
     },
     {
         name: "Vineet Shukla (Y17)",
         work: "Groww",
         text: "Greetings Everyone! I am Vineet Shukla, a Y17 Civil Engineering student currently hustling through all major formalities of the final semester at IITK. I am at home, for now, struggling to recover my lost sense of smell and taste, waiting for my joining, and convincing people to write in my yearbook.",
         dp: "/img/Student-section/placement/Vineet-Shukla.jpg",
-        link:"/Vineet_Shukla"
+        link:"/Vineet_Shukla",
+        thumbnail:"/img/Student-section/placement/thumbnail/11.jpg",
     },
     {
         name: "Krishna Deo (Y17)",
         work: "Sutra Management Consultancies",
         text: "Greetings Everyone! I am Krishna Deo(Y17). I completed my Btech in Civil engineering(from IITK, obviously) this year. I will be joining Sutra Management Consultancies as a Business Analyst. In my 3rd Year, I did my Internship at ZestMoney as a Product Management intern(SPO se lagi thi). ",
         dp: "/img/Student-section/placement/Krishna-Deo.jpg",
-        link:"/Krishna_Deo"
+        link:"/Krishna_Deo",
+        thumbnail:"/img/Student-section/placement/thumbnail/12.jpg",
     },
     {
         name: "Gourav Kamboj (Y17)",
         work: "HSBC",
         text: "Placement and Internship are the most frequent words that keep howling in our mind probably after the 2nd year and sometimes leaving no space to think of anything else. This phase comes in the life of the majority of us if not all.",
         dp: "/img/Student-section/placement/Gourav-Kamboj.jpg",
-        link:"/Gaurav_Kamboj"
+        link:"/Gaurav_Kamboj",
+        thumbnail:"/img/Student-section/placement/thumbnail/13.jpg",
     },
     {
         name: "Rahul Gauriyar (Y16)",
         work: "JioSaavn",
         text: "Greetings Everyone! I am Rahul Gauriyar, Y16 BT-MT Dual Degree student, placed in JioSaavn as Software Engineer this year. I spent the first two years in a carefree, casual manner relishing every moment, getting high on TV series",
         dp: "/img/Student-section/placement/Rahul-Gauriyar.jpg",
-        link:"/Rahul_Gauriyar"
+        link:"/Rahul_Gauriyar",
+        thumbnail:"/img/Student-section/placement/thumbnail/14.jpg",
     },
     {
         name: "Mudit Kumar (Y17)",
         work: "PayU",
         text: "Greetings Everyone! I am Mudit Kumar, Y17 B.Tech in Civil Engineering student, placed in PayU as a Data Scientist. Talking about my academic background, I had a pretty bad start as I had an SPI of 6.6 in my 1st sem.",
         dp: "/img/Student-section/placement/mudit-kumar-y17.jpeg",
-        link:"/Mudit_Kumar"
+        link:"/Mudit_Kumar",
+        thumbnail:"/img/Student-section/placement/thumbnail/15.jpg",
     },
     {
         name: "Dishant Parewa",
         work: "IIFL",
         text: "Greetings Everyone! My name is Dishant Parewa, currently placed as a Software Developer at IIFL and today I am going to share my story. I hope many students will relate to it.",
         dp: "/img/Student-section/placement/dishant-parewa-y17.jpg",
-        link:"/Dishant_Parewa"
+        link:"/Dishant_Parewa",
+        thumbnail:"/img/Student-section/placement/thumbnail/16.jpg",
     },
 
 ]
