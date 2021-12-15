15357e5032f2423d5ae961220092d9490234200a
Aman <amanks20@iitk.ac.in>
2021-12-16 00:12:35 +0530

    Update placement.js

--- database/placement.js
+++ database/placement.js
@@ -3,7 +3,7 @@ var chats = [
     name: "Dishant Parewa (Y17)",
     work: "IIFL",
     text: "Greetings Everyone! My name is Dishant Parewa, currently placed as a Software Developer at IIFL and today I am going to share my story. I hope many students will relate to it.",
-    dp: "/img/Student-section/placement/dp16.jpg",
+    dp: "/img/Student-section/placement/dp/16.jpg",
     link: "/Dishant_Parewa",
     thumbnail: "/img/Student-section/placement/thumbnail/16.jpg",
     og: "Dishant_Parewa"
@@ -13,7 +13,7 @@ var chats = [
     name: "Mudit Kumar (Y17)",
     work: "PayU",
     text: "Greetings Everyone! I am Mudit Kumar, Y17 B.Tech in Civil Engineering student, placed in PayU as a Data Scientist. Talking about my academic background, I had a pretty bad start as I had an SPI of 6.6 in my 1st sem.",
-    dp: "/img/Student-section/placement/dp15.jpg",
+    dp: "/img/Student-section/placement/dp/15.jpg",
     link: "/Mudit_Kumar",
     thumbnail: "/img/Student-section/placement/thumbnail/15.jpg",
     og: "Mudit_Kumar"
@@ -23,7 +23,7 @@ var chats = [
     name: "Rahul Gauriyar (Y16)",
     work: "JioSaavn",
     text: "Greetings Everyone! I am Rahul Gauriyar, Y16 BT-MT Dual Degree student, placed in JioSaavn as Software Engineer this year. I spent the first two years in a carefree, casual manner relishing every moment, getting high on TV series",
-    dp: "/img/Student-section/placement/dp14.jpg",
+    dp: "/img/Student-section/placement/dp/14.jpg",
     link: "/Rahul_Gauriyar",
     thumbnail: "/img/Student-section/placement/thumbnail/14.jpg",
     og: "Rahul_Gauriyar"
@@ -33,7 +33,7 @@ var chats = [
     name: "Gourav Kamboj (Y17)",
     work: "HSBC",
     text: "Placement and Internship are the most frequent words that keep howling in our mind probably after the 2nd year and sometimes leaving no space to think of anything else. This phase comes in the life of the majority of us if not all.",
-    dp: "/img/Student-section/placement/dp13.jpg",
+    dp: "/img/Student-section/placement/dp/13.jpg",
     link: "/Gaurav_Kamboj",
     thumbnail: "/img/Student-section/placement/thumbnail/13.jpg",
     og: "Gaurav_Kamboj"
@@ -43,7 +43,7 @@ var chats = [
     name: "Krishna Deo (Y17)",
     work: "Sutra Management Consultancies",
     text: "Greetings Everyone! I am Krishna Deo(Y17). I completed my Btech in Civil engineering(from IITK, obviously) this year. I will be joining Sutra Management Consultancies as a Business Analyst. In my 3rd Year, I did my Internship at ZestMoney as a Product Management intern(SPO se lagi thi). ",
-    dp: "/img/Student-section/placement/dp12.jpg",
+    dp: "/img/Student-section/placement/dp/12.jpg",
     link: "/Krishna_Deo",
     thumbnail: "/img/Student-section/placement/thumbnail/12.jpg",
     og: "Krishna_Deo"
@@ -53,7 +53,7 @@ var chats = [
     name: "Vineet Shukla (Y17)",
     work: "Groww",
     text: "Greetings Everyone! I am Vineet Shukla, a Y17 Civil Engineering student currently hustling through all major formalities of the final semester at IITK. I am at home, for now, struggling to recover my lost sense of smell and taste, waiting for my joining, and convincing people to write in my yearbook.",
-    dp: "/img/Student-section/placement/dp11.jpg",
+    dp: "/img/Student-section/placement/dp/11.jpg",
     link: "/Vineet_Shukla",
     thumbnail: "/img/Student-section/placement/thumbnail/11.jpg",
     og: "Vineet_Shukla"
@@ -63,7 +63,7 @@ var chats = [
     name: "Rajat Ghoshal (Y17)",
     work: "Hilabs Inc.",
     text: "Hello Everyone! I am Rajat Ghoshal, a Final Year Undergraduate (Y17) in Civil Engineering, hoping for minors in IME and ML (a retrospective one :p). About academics - with a CPI just enough for distinction",
-    dp: "/img/Student-section/placement/dp10.jpg",
+    dp: "/img/Student-section/placement/dp/10.jpg",
     link: "/Rajat_Ghoshal",
     thumbnail: "/img/Student-section/placement/thumbnail/10.jpg",
     og: "Rajat_Ghoshal"
@@ -73,7 +73,7 @@ var chats = [
     name: "Samyak Jain (Y17)",
     work: "BFSI",
     text: "Hello Everyone! I am Samyak Jain, a final year undergraduate in Civil Engineering. I am currently placed at Navi technologies, a startup in the BFSI industry, as a Software developer.",
-    dp: "/img/Student-section/placement/dp9.jpg",
+    dp: "/img/Student-section/placement/dp/9.jpg",
     link: "/Samyak_Jain",
     thumbnail: "/img/Student-section/placement/thumbnail/9.jpg",
     og: "Samyak_Jain"
@@ -83,7 +83,7 @@ var chats = [
     name: "Shashi Shekhar (Y17)",
     work: "Blackrock",
     text: "Hi people! Let me share my story. So, I am Shashi Shekhar. I got placed in Blackrock on Day 1 during the placement season 20-21. My extra-curricular involvements changed with every year, but the major ones have been Manager- Research, Academics and Career Council and Senator, Y17.",
-    dp: "/img/Student-section/placement/dp8.jpg",
+    dp: "/img/Student-section/placement/dp/8.jpg",
     link: "/Shashi_Shekhar",
     thumbnail: "/img/Student-section/placement/thumbnail/8.jpg",
     og: "Shashi_Shekhar"
@@ -93,7 +93,7 @@ var chats = [
     name: "Vishal Agarwal (Y17)",
     work: "MTX",
     text: "Hello Everyone! My name is Vishal Agarwal (Y17, CE), currently placed at MTX Group and today I am going to give you a student’s perspective, something really honest and straightforward because that’s an element often missing in the talks that are given by the companies.",
-    dp: "/img/Student-section/placement/dp7.jpg",
+    dp: "/img/Student-section/placement/dp/7.jpg",
     link: "/Vishal_Agarwal",
     thumbnail: "/img/Student-section/placement/thumbnail/7.jpg",
     og: "Vishal_Agarwal"
@@ -103,7 +103,7 @@ var chats = [
     name: "Krishan Kumar (Y17)",
     work: "Sterlite Technology",
     text: "Starting with a brief intro of mine, I am Krishan Kumar, Y17 BT Civil Engineering. I got Placed on Day 2 in a Company named Sterlite Technology in my very first interview.",
-    dp: "/img/Student-section/placement/dp6.jpg",
+    dp: "/img/Student-section/placement/dp/6.jpg",
     link: "/Krishan_Kumar",
     thumbnail: "/img/Student-section/placement/thumbnail/6.jpg",
     og: "Krishan_Kumar"
@@ -113,7 +113,7 @@ var chats = [
     name: "Dishaa Rajora (Y17)",
     work: "PWC",
     text: "Before I begin with the story of my placement journey, let us have introductions first. Hi. I am Dishaa Rajora, final year undergraduate majoring in Civil Engineering (wow, this introduction gave me formal-email-to-professor-don’t-know-how-to-begin-the-email feel).",
-    dp: "/img/Student-section/placement/dp5.jpg",
+    dp: "/img/Student-section/placement/dp/5.jpg",
     link: "/Dishaa_Rajora",
     thumbnail: "/img/Student-section/placement/thumbnail/5.jpg",
     og: "Dishaa_Rajora"
@@ -123,7 +123,7 @@ var chats = [
     name: "Ashutosh Ranjan (Y17)",
     work: "TATA Projects Limited",
     text: "Hello Everyone! My name is Ashutosh Ranjan, Y17, BTech in Civil Engineering, IITK. First, I will give a brief introduction about my curriculum. I am a fourth-year student and got placed in TATA Projects Limited in placement season 2020-21.",
-    dp: "/img/Student-section/placement/dp4.jpg",
+    dp: "/img/Student-section/placement/dp/4.jpg",
     link: "/Ashutosh_Ranjan",
     thumbnail: "/img/Student-section/placement/thumbnail/4.jpg",
     og: "Ashutosh_Ranjan"
@@ -133,7 +133,7 @@ var chats = [
     name: "Chitwan Garg (Y16)",
     work: "HSBC",
     text: "Hello Everyone! Myself Chitwan Garg Y16, B-Tech graduate in Civil Engineering and a Double Major in Economics. I am a guy who did not have any significant extracurricular involvements during my first year except for those that were made compulsory to remain patriotic to our mother hall with hearts full of hall feeling :p (I enjoyed the escapes from HEC that I made with some of my wingies during takneek).",
-    dp: "/img/Student-section/placement/dp3.jpg",
+    dp: "/img/Student-section/placement/dp/3.jpg",
     link: "/Chitwan_Garg",
     thumbnail: "/img/Student-section/placement/thumbnail/3.jpg",
     og: "Chitwan_Garg"
@@ -143,9 +143,9 @@ var chats = [
     name: "Shuaib Alam (Y18 PG)",
     work: "Hero Future Energies (New Delhi)",
     text: "Hello Everyone! Myself SHUAIB ALAM. I completed my Graduation from JAMIA MILLIA ISLAMIA, NEW DELHI in CIVIL ENGINEERING.",
-    dp: "/img/Student-section/placement/dp2.jpg",
+    dp: "/img/Student-section/placement/dp/2.jpg",
     link: "/Shuaib_Alam",
-    thumbnail: "/img/Student-section/placement/dp2.jpg",
+    thumbnail: "/img/Student-section/placement/dp/2.jpg",
     og: "Shuaib_Alam"
   },
 
@@ -153,7 +153,7 @@ var chats = [
     name: "Jeetendra Kumar Patel (Y16)",
     work: "IOCL",
     text: "Let me first introduce myself. I am Jeetendra Kumar Patel (Y16), B.Tech in CE, IITK. Currently, I am working in IOCL, I got placed in Phase 2 last year. ",
-    dp: "/img/Student-section/placement/dp1.jpg",
+    dp: "/img/Student-section/placement/dp/1.jpg",
     link: "/Jeetendra_Kumar_Patel",
     thumbnail: "/img/Student-section/placement/thumbnail/1.jpg",
     og: "Jeetendra_Kumar_Patel"
