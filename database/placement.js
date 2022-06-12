b89d2708cd016564a664202f18786579851e0de3
Aman <amanks20@iitk.ac.in>
2022-06-12 17:22:14 +0530

    Piyush Senwar

--- database/placement.js
+++ database/placement.js
@@ -4,9 +4,9 @@ var chats = [
     work: "Micron Technology",
     text: "Hi everyone, this is Piyush Senwar, who will be joining Micron Technology as an Associate Data Science Engineer. Through this postulate, I would like to take you to my placement journey, what challenges I faced, a bit of my journey on how I got into data science,",
     dp: "/img/Student-section/placement-postulates/dp/21.jpg",
-    link: "/Piyush_Senwal",
+    link: "/Piyush_Senwar",
     thumbnail: "/img/Student-section/placement-postulates/thumbnail/21.jpg",
-    og: "Piyush_Senwal"
+    og: "Piyush_Senwar"
   },
   {
     name: "Mradul Chaudhary (Y18)",
