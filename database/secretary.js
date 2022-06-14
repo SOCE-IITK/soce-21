01f21bc74a6eecd7e679c13ec17b45bfa0ec5389
Aman <amanks20@iitk.ac.in>
2022-06-14 11:59:32 +0530

    update list

--- database/secretary.js
+++ database/secretary.js
@@ -204,55 +204,7 @@ var secy = [{
     linkedin: "https://www.linkedin.com/in/shivangi-nauhwar-044b12217/",
     mail: "mailto:shivangi20@iitk.ac.in",
     image: "img/team/m4.jpg"
-  },
-
-
-  // {
-  //     name: "Akshat Arya",
-  //     post: "Secretary, Web Development",
-  //     fb: "https://www.facebook.com/profile.php?id=100057133705422",
-  //     insta: "https://www.instagram.com/forget_that__/",
-  //     linkedin: "https://www.linkedin.com/in/akshat-arya-4b0318202/",
-  //     mail: "mailto:akshatarya20@iitk.ac.in",
-  //     image: "img/team/w1.jpg"
-  // },
-  // {
-  //     name: "Faiza khan",
-  //     post: "Secretary, Web Development",
-  //     fb: "https://www.facebook.com/profile.php?id=100037231279653",
-  //     insta: "https://www.instagram.com/faizak31/",
-  //     linkedin: "https://www.linkedin.com/in/faiza-khan-b21b71200/",
-  //     mail: "mailto:faizakhan20@iitk.ac.in",
-  //     image: "img/team/w2.jpg"
-  // },
-  // {
-  //     name: "Kumar Abhishek",
-  //     post: "Secretary, Web Development",
-  //     fb: "https://www.facebook.com/kabhishek20/",
-  //     insta: "https://www.instagram.com/k_abhishek.15/",
-  //     linkedin: "https://www.linkedin.com/in/kumar-abhishek-629303216/",
-  //     mail: "mailto:kabhishek20@iitk.ac.in",
-  //     image: "img/team/w3.jpg"
-  // },
-  // {
-  //     name: "Saurabh Sahay",
-  //     post: "Secretary, Web Development",
-  //     fb: "https://www.facebook.com/saurabh.sahay.395",
-  //     insta: "https://www.instagram.com/sahay.saurabh/",
-  //     linkedin: "https://www.linkedin.com/in/saurabh-sahay-b20797197/",
-  //     mail: "mailto:ssahay20@iitk.ac.in",
-  //     image: "img/team/w4.jpg"
-  // },
-  // {
-  //     name: "Yuvraj Singh",
-  //     post: "Secretary, Web Development",
-  //     fb: "https://www.facebook.com/profile.php?id=100033277658507",
-  //     insta: "https://www.instagram.com/__yuvisofficial/",
-  //     linkedin: "https://www.linkedin.com/in/yuvraj-singh-b6955b1aa",
-  //     mail: "mailto:syuvraj20@iitk.ac.in",
-  //     image: "img/team/w5.jpg"
-  // }
-
+  }
 ]
 
 module.exports = secy;
