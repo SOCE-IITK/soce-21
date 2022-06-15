7790ba9ef500313208f2b0ab1182172541b8b740
Aman <amanks20@iitk.ac.in>
2022-06-15 21:25:57 +0530

    secy init

--- database/secretary.js
+++ database/secretary.js
@@ -1,30 +1,21 @@
 var secy = [{
-    name: "TO BE RELEASED SOON",
-    post: "",
-    fb: "https://www.facebook.com/",
-    insta: "https://www.instagram.com/",
-    linkedin: "https://www.linkedin.com/in/",
-    mail: "mailto:21@iitk.ac.in",
+    name: "Aman Khilani",
+    post: "Secretary Content",
+    fb: "https://www.facebook.com/aman.khilani.5",
+    insta: "https://www.instagram.com/main.ak56/",
+    linkedin: "https://www.linkedin.com/in/aman-khilani",
+    mail: "mailto:amanrk21@iitk.ac.in",
     image: "img/team/c1.jpg"
   },
-  // {
-  //   name: "",
-  //   post: "Secretary, ",
-  //   fb: "https://www.facebook.com/",
-  //   insta: "https://www.instagram.com/",
-  //   linkedin: "https://www.linkedin.com/in/",
-  //   mail: "mailto:21@iitk.ac.in",
-  //   image: "img/team/c1.jpg"
-  // },
-  // {
-  //   name: "",
-  //   post: "Secretary, ",
-  //   fb: "https://www.facebook.com/",
-  //   insta: "https://www.instagram.com/",
-  //   linkedin: "https://www.linkedin.com/in/",
-  //   mail: "mailto:21@iitk.ac.in",
-  //   image: "img/team/c1.jpg"
-  // },
+  {
+    name: "Arpit Raj",
+    post: "Secretary Content",
+    fb: "https://www.facebook.com/profile.php?id=100074288032644",
+    insta: "https://www.instagram.com/main.ak56/",
+    linkedin: "https://www.instagram.com/_arpit.raj_/",
+    mail: "mailto:arpitr21@iitk.ac.in",
+    image: "img/team/c2.jpg"
+  },
 ]
 
 module.exports = secy;
