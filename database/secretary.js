fc38bd56367401699551166fdff3174fe3167b90
Aman <amanks20@iitk.ac.in>
2022-06-16 10:46:16 +0530

    govind summer project

--- database/secretary.js
+++ database/secretary.js
@@ -7,15 +7,15 @@ var secy = [{
     mail: "mailto:amanrk21@iitk.ac.in",
     image: "img/team/c1.jpg"
   },
-  {
-    name: "Arpit Raj",
-    post: "Secretary Content",
-    fb: "https://www.facebook.com/profile.php?id=100074288032644",
-    insta: "https://www.instagram.com/main.ak56/",
-    linkedin: "https://www.instagram.com/_arpit.raj_/",
-    mail: "mailto:arpitr21@iitk.ac.in",
-    image: "img/team/c2.jpg"
-  },
+//   {
+//     name: "Arpit Raj",
+//     post: "Secretary Content",
+//     fb: "https://www.facebook.com/profile.php?id=100074288032644",
+//     insta: "https://www.instagram.com/main.ak56/",
+//     linkedin: "https://www.instagram.com/_arpit.raj_/",
+//     mail: "mailto:arpitr21@iitk.ac.in",
+//     image: "img/team/c2.jpg"
+//   },
 ]
 
 module.exports = secy;
