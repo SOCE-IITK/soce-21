721f141c856157c72ea1c294c777e419d000e8c8
Aman <amanks20@iitk.ac.in>
2022-06-16 11:06:57 +0530

    update

--- database/secretary.js
+++ database/secretary.js
@@ -1,21 +1,328 @@
 var secy = [{
-    name: "Aman Khilani",
-    post: "Secretary Content",
-    fb: "https://www.facebook.com/aman.khilani.5",
-    insta: "https://www.instagram.com/main.ak56/",
-    linkedin: "https://www.linkedin.com/in/aman-khilani",
-    mail: "mailto:amanrk21@iitk.ac.in",
-    image: "img/team/c1.jpg"
-  },
-//   {
-//     name: "Arpit Raj",
-//     post: "Secretary Content",
-//     fb: "https://www.facebook.com/profile.php?id=100074288032644",
-//     insta: "https://www.instagram.com/main.ak56/",
-//     linkedin: "https://www.instagram.com/_arpit.raj_/",
-//     mail: "mailto:arpitr21@iitk.ac.in",
-//     image: "img/team/c2.jpg"
-//   },
+  name: "Aman Khilani",
+  post: "Secretary Content",
+  fb: "https://www.facebook.com/aman.khilani.5",
+  insta: "https://www.instagram.com/main.ak56/",
+  linkedin: "https://www.linkedin.com/in/aman-khilani",
+  mail: "mailto:amanrk21@iitk.ac.in",
+  image: "img/team/c1.jpg"
+},
+{
+  name: "Arpit Raj",
+  post: "Secretary Content",
+  fb: "https://www.facebook.com/profile.php?id=100074288032644",
+  insta: "https://www.instagram.com/_arpit.raj_/",
+  linkedin: "https://www.linkedin.com/in/arpit-raj-25416a160",
+  mail: "mailto:arpitr21@iitk.ac.in",
+  image: "img/team/c2.jpg"
+},
+{
+  name: "Eshika yadav",
+  post: "Secretary Content",
+  fb: "https://www.facebook.com/eshika.yadav.9083",
+  insta: "http://instagram.com/eshika_532",
+  linkedin: "https://www.linkedin.com/in/eshika-yadav-a7a9aa23a",
+  mail: "mailto:eshikay21@iitk.ac.in",
+  image: "img/team/c3.jpg"
+},
+{
+  name: "Muskan Gautam",
+  post: "Secretary Content",
+  fb: "",
+  insta: "https://www.instagram.com/muskangautam_2203/",
+  linkedin: "",
+  mail: "mailto:muskang21@iitk.ac.in",
+  image: "img/team/c4.jpg"
+},
+{
+  name: "Prasang Vashishtha",
+  post: "Secretary Content",
+  fb: " ",
+  insta: "",
+  linkedin: "",
+  mail: "mailto:prasangv21@iitk.ac.in",
+  image: "img/team/c5.jpg"
+},
+{
+  name: "Sandipan Dutta",
+  post: "Secretary Content",
+  fb: "https://www.facebook.com/profile.php?id=100070465247144",
+  insta: "https://www.instagram.com/imsandipandutta03/",
+  linkedin: "https://www.linkedin.com/in/sandipan-dutta-6a03b6237/",
+  mail: "mailto:sadipand21@iitk.ac.in",
+  image: "img/team/c6.jpg"
+},
+{
+  name: "Shruti Goyal",
+  post: "Secretary Content",
+  fb: "https://www.facebook.com/profile.php?id=100073368438974",
+  insta: "https://instagram.com/shrutii_goyal_?igshid=YmMyMTA2M2Y=",
+  linkedin: "www.linkedin.com/in/shruti-goyal-581064224",
+  mail: "mailto:shrutig21@iitk.ac.in",
+  image: "img/team/c7.jpg"
+},
+{
+  name: "Arnesh Dadhich",
+  post: "Secretary Design",
+  fb: " ",
+  insta: "",
+  linkedin: "",
+  mail: "mailto:",
+  image: "img/team/d1.jpg"
+},
+{
+  name: "Dilkesh Meena",
+  post: "Secretary Design",
+  fb: "https://www.facebook.com/profile.php?id=100075331086946",
+  insta: "",
+  linkedin: "",
+  mail: "mailto:dilkeshm21@iitk.ac.in",
+  image: "img/team/d2.jpg"
+},
+{
+  name: "Harsh Vardhan ",
+  post: "Secretary Design",
+  fb: "https://www.facebook.com/profile.php?id=100074225526914",
+  insta: "https://www.instagram.com/__harshv.__10/?hl=en",
+  linkedin: "https://www.linkedin.com/in/harsh-vardhan-a1601922a",
+  mail: "mailto:harshv21@iitk.ac.in",
+  image: "img/team/d3.jpg"
+},
+{
+  name: "Kurva Shalini",
+  post: "Secretary Design",
+  fb: "https://www.facebook.com/profile.php?id=100075245501340",
+  insta: "https://instagram.com/shaliniii_2206?igshid=YmMyMTA2M2Y=",
+  linkedin: "https://www.linkedin.com/in/shalini-kurva-a05640232",
+  mail: "mailto:shalini21@iitk.ac.in",
+  image: "img/team/c5.jpg"
+},
+{
+  name: "Prateek Mishra",
+  post: "Secretary Design",
+  fb: " ",
+  insta: "",
+  linkedin: "",
+  mail: "mailto:mprateek21@iitk.ac.in",
+  image: "img/team/c5.jpg"
+},
+{
+  name: "Sanchit Hari",
+  post: "Secretary Design",
+  fb: "https://www.facebook.com/fabSANCHIT",
+  insta: "",
+  linkedin: "https://www.linkedin.com/in/sanchithari",
+  mail: "mailto:sanchit21@iitk.ac.in",
+  image: "img/team/c5.jpg"
+},
+{
+  name: "Sanju Kakerwar",
+  post: "Secretary Design",
+  fb: " ",
+  insta: "www.instagram.com/_.sanjukakerwar._/",
+  linkedin: "",
+  mail: "mailto:sanjuk21@iitk.ac.in",
+  image: "img/team/c5.jpg"
+},
+{
+  name: "Abhiraj Singh",
+  post: "Secretary Events",
+  fb: "https://www.facebook.com/abhiraj.singh.96387 ",
+  insta: "https://www.instagram.com/03abhi2003/",
+  linkedin: "https://www.linkedin.com/in/abhiraj-singh-24072a219",
+  mail: "mailto:abhiraj21@iitk.ac.in",
+  image: "img/team/e1.jpg"
+},
+{
+  name: "Akarsha",
+  post: "Secretary Events",
+  fb: "https://www.facebook.com/profile.php?id=100074795215350",
+  insta: "https://instagram.com/akarsha_13?igshid=YmMyMTA2M2Y=",
+  linkedin: "https://www.linkedin.com/in/akarsha-yadav-077346226",
+  mail: "mailto:akarsha21@iitk.ac.in",
+  image: "img/team/e2.jpg"
+},
+{
+  name: "Dharmraj",
+  post: "Secretary Events",
+  fb: "https://www.facebook.com/dharm.raj.71653",
+  insta: "https://instagram.com/dharmraj_5565?igshid=YmMyMTA2M2Y=",
+  linkedin: "https://www.linkedin.com/in/dharm-raj-30760b22a",
+  mail: "mailto:dharmraj21@iitk.ac.in",
+  image: "img/team/e3.jpg"
+},
+{
+  name: "Manuja Pandey",
+  post: "Secretary Events",
+  fb: " https://www.facebook.com/manuja.pandey.1",
+  insta: "",
+  linkedin: "",
+  mail: "mailto:manujap21@iitk.ac.in",
+  image: "img/team/e4.jpg"
+},
+{
+  name: "Mayank Shekhar",
+  post: "Secretary Events",
+  fb: "https://www.facebook.com/mayank.shekhar.710",
+  insta: "https://instagram.com/mayank___shekhar?igshid=YmMyMTA2M2Y=",
+  linkedin: "https://www.linkedin.com/in/mayank-shekhar-a71589232",
+  mail: "mailto:mshekhar21@iitk.ac.in",
+  image: "img/team/e5.jpg"
+},
+{
+  name: "Saurabh Meena",
+  post: "Secretary Events",
+  fb: "https://www.facebook.com/profile.php?id=100074976683799",
+  insta: "https://instagram.com/master_saurabh001?igshid=YmMyMTA2M2Y=",
+  linkedin: "https://www.linkedin.com/in/saurabh-meena-94600223b",
+  mail: "mailto:msaurabh21@iitk.ac.in",
+  image: "img/team/e6.jpg"
+},
+{
+  name: "Shambhavi Agarwal",
+  post: "Secretary Events",
+  fb: "https://www.facebook.com/profile.php?id=100075231073918",
+  insta: "https://instagram.com/i_shambhavi?igshid=YmMyMTA2M2Y=",
+  linkedin: "https://www.linkedin.com/in/shambhavi-agarwal-15b47b242",
+  mail: "mailto:shambhavi21@iitk.ac.in",
+  image: "img/team/e7.jpg"
+},
+{
+  name: "Srishti",
+  post: "Secretary Events",
+  fb: "https://www.facebook.com/srishti.s.10",
+  insta: "https://www.instagram.com/eunoia_ish/",
+  linkedin: "https://www.linkedin.com/in/srishti-s-557129236/",
+  mail: "mailto:srishti21@iitk.ac.in",
+  image: "img/team/e8.jpg"
+},
+{
+  name: "Aashruti",
+  post: "Secretary Web Development",
+  fb: "https://www.facebook.com/profile.php?id=100075348576412",
+  insta: "https://www.instagram.com/invites/contact/?i=1pvjy1qrcpo18&utm_content=kevllzc",
+  linkedin: "",
+  mail: "mailto:aashruti21@iitk.ac.in ",
+  image: "img/team/w1.jpg"
+},
+{
+  name: "Ambuj Pyne",
+  post: "Secretary Web Development",
+  fb: "https://www.facebook.com/profile.php?id=100075328710987",
+  insta: "https://www.instagram.com/ambujpyne/",
+  linkedin: "https://www.linkedin.com/in/ambuj-pyne-427b9022b",
+  mail: "mailto:ambujp21@iitk.ac.in",
+  image: "img/team/w2.jpg"
+},
+{
+  name: "Arpit Kaithwar",
+  post: "Secretary Web Development",
+  fb: "https://www.facebook.com/profile.php?id=100075288250644",
+  insta: "https://www.instagram.com/arpit_kaithwar_/",
+  linkedin: "http://www.linkedin.com/in/arpit-kaithwar-ak83035",
+  mail: "mailto:akaithwar21@iitk.ac.in",
+  image: "img/team/w3.jpg"
+},
+{
+  name: "MD Rahbar",
+  post: "Secretary Web Development",
+  fb: "https://www.facebook.com/rahbar.khan.355",
+  insta: "https://instagram.com/rowdyrahbar",
+  linkedin: "https://www.linkedin.com/in/md-rahbar-0576071a7",
+  mail: "mailto:mdrahbar21@iitk.ac.in",
+  image: "img/team/w4.jpg"
+},
+{
+  name: "Saurav Kumar",
+  post: "Secretary Web Development",
+  fb: "https://www.facebook.com/profile.php?id=100035038578902",
+  insta: "https://www.instagram.com/kr_saurav35/",
+  linkedin: "https://www.linkedin.com/in/saurav-kumar-1894a5226",
+  mail: "mailto:sauravkr21@iitk.ac.in",
+  image: "img/team/w5.jpg"
+},
+{
+  name: "Zoya Manjer",
+  post: "Secretary Web Development",
+  fb: "https://www.facebook.com/profile.php?id=100075140295379",
+  insta: "https://www.instagram.com/invites/contact/?i=176y5tfiw2etu&utm_content=3kxm38f",
+  linkedin: "https://www.linkedin.com/in/zoya-manjer-5a347b242",
+  mail: "mailto:zoyam21@iitk.ac.in",
+  image: "img/team/w6.jpg"
+},
+{
+  name: "Aadya Umrao",
+  post: "Secretary Marketing & Finance",
+  fb: "https://www.facebook.com/profile.php?id=100082214229058",
+  insta: "https://www.instagram.com/invites/contact/?i=1cxfnglh9qn6e&utm_content=kejcmen",
+  linkedin: "https://www.linkedin.com/in/aadya-umrao-b63b9922b",
+  mail: "mailto:aadyau21@iitk.ac.in",
+  image: "img/team/m1.jpg"
+},
+{
+  name: "Arvind Mehta",
+  post: "Secretary Marketing & Finance",
+  fb: "https://www.facebook.com/profile.php?id=100053896070044",
+  insta: "",
+  linkedin: "",
+  mail: "mailto:amehta21@iitk.ac.in",
+  image: "img/team/m2.jpg"
+},
+{
+  name: "Ashok Singh Naruka",
+  post: "Secretary Marketing & Finance",
+  fb: "https://www.facebook.com/ashok.naruka.520",
+  insta: "https://www.instagram.com/_asn14_the_gr8/",
+  linkedin: "https://www.linkedin.com/in/ashok-singh-naruka-a196a6223",
+  mail: "mailto:ashoksn21@iitk.ac.in",
+  image: "img/team/m3.jpg"
+},
+{
+  name: "Enna Gupta",
+  post: "Secretary Marketing & Finance",
+  fb: "https://www.facebook.com/enna.gupta.104",
+  insta: "https://instagram.com/enna_1902",
+  linkedin: "https://www.linkedin.com/in/enna-gupta-013340240",
+  mail: "mailto:ennagupta21@iitk.ac.in ",
+  image: "img/team/m4.jpg"
+},
+{
+  name: "Ishu Singh",
+  post: "Secretary Marketing & Finance",
+  fb: "",
+  insta: "",
+  linkedin: "",
+  mail: "mailto:",
+  image: "img/team/m5.jpg"
+},
+{
+  name: "Pratham Srivastava",
+  post: "Secretary Marketing & Finance",
+  fb: "https://www.facebook.com/profile.php?id=100074946619797",
+  insta: "https://instagram.com/1prthm_srvstv?igshid=YmMyMTA2M2Y=",
+  linkedin: "https://www.linkedin.com/in/pratham-srivastava-a70085230",
+  mail: "mailto:prathams21@iitk.ac.in",
+  image: "img/team/m6.jpg"
+ },
+{
+  name: "Shrey Patel",
+  post: "Secretary Marketing & Finance",
+  fb: "https://www.facebook.com/shrey.patel.5243",
+  insta: "https://www.instagram.com/shreypatel1408/",
+  linkedin: "https://www.linkedin.com/in/shrey-patel-830bb164/",
+  mail: "mailto:shreypatel21@iitk.ac.in",
+  image: "img/team/m7.jpg"
+},
+{
+  name: "Vanshika Yadav",
+  post: "Secretary Marketing & Finance",
+  fb: "https://www.facebook.com/profile.php?id=100075371045211",
+  insta: "https://www.instagram.com/vanshu_ydav/",
+  linkedin: "https://www.linkedin.com/in/vanshika-yadav-468a58233",
+  mail: "mailto:vanshika21@iitk.ac.in",
+  image:"img/team/m8.jpg"
+},
+
 ]
 
 module.exports = secy;
